cce51cc4c779dbfd8462ea116f419105bd0e5531
unknown <abhishekguptaatweb17@gmail.com>
2019-01-20 23:44:40 +0530

    add components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const cLink =`https://researchkernel.org`;
 
 const Navbar1 = styled.div`
+
   border-style: solid;
   border-width: 0px 0px 1px 0px;
   font-weight: bold;
@@ -14,9 +15,8 @@ const Navbar1 = styled.div`
   `;
 
 const Link = styled.a`
-  //float: right;
-  position: relative;
   
+  position: relative;
   padding: 0rem .5rem 1.2rem .5rem;  
   display: inline-block;
   cursor: pointer;
@@ -28,14 +28,57 @@ const Link = styled.a`
 
 const SearchBar = styled.input`
 position: relative;
-  float: right;
-  padding: 0rem .5rem 1.2rem .5rem;  
+// 
+font-size: 14px;
+width: 20%;
+display: table;
+  padding: 0rem .5rem 1.2rem .5rem;
+  margin-left: 350px;  
  margin-bottom: 9px;
+ margin-right: 450px;
  display: inline-block;
  `;
+const Dropdown = styled.select.attrs({
+  name: "Profile"
+})`
+ font-size: inherit; 
+   padding: 0rem .5rem 1.2rem .5rem;
+  border: none;
+  outline: none;
+  display: inline-block;
+  cursor: pointer;
+  background-color: inherit;
+  
+  //font: 0.8em "Montserrat", sans-serif;
+  //height: 3em;
+  //width: 18em; 
+`
 
-const placeholder = "Search Research Papers";
+const Profile = styled.button`
+  
+   font-size: inherit; 
+   padding: 0rem .5rem 1.2rem .5rem;
+  border: none;
+  display: inline-block;
+  cursor: pointer;
+  background-color: inherit;
+  //font-family: inherit;
+  //margin: 0;
+`;
+
+const ProfileContent = styled.div`
+  
+// .navbar a:hover, .dropdown:hover .dropbtn {
+  ${Profile}: hover{
+    display: inline-grid;
+   background-color: #fafafa;
+    color: #007bff;
+  }
+`;
+
+
 const popularLink=`${cLink}/popular`;
+const allLink = `${cLink}/all`;
 
 export default class Navbar extends React.Component {
   
@@ -43,11 +86,22 @@ export default class Navbar extends React.Component {
     return (
       <Navbar1>
       Logo
-      <Link href={popularLink}> Popular </Link>
-        <Link href={popularLink}> Popular </Link>
-        <SearchBar placeholder={placeholder} />
-        </Navbar1>
-    )}
-}
+      <SearchBar placeholder="Search Research Papers" />
+      <Link href={popularLink} > Popular </Link>
+        <Link href={allLink} > All </Link>
+     
+     
+        // <Profile>
+      // <ProfileContent>
+      // </ProfileContent></Profile>        
+
+        < Dropdown >
+          <option value="val1" ><a href=""></a></option>
+          <option value="val2" selected>Night Mode</option>
+          <option value="val3" >Login</option>
+          <option value="val4"></option>
+        </Dropdown >    </Navbar1>
+    )};
+};
 
 
